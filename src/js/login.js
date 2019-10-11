83f7d24aca69982a0d8e6990584fcb5c81c55455
jpcaltabiano <jpcaltabiano@wpi.edu>
2019-10-11 14:57:52 -0400

    minifed

--- src/js/login.js
+++
@@ -1,7 +0,0 @@
-//renders the components for the login page
-
-import React from "react";
-import ReactDOM from "react-dom";
-import LoginApp from "./LoginApp";
-
-ReactDOM.render(<LoginApp />, document.getElementById("root"));
